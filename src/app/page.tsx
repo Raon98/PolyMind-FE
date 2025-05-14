8aab1320aa302fc11106919fcdcac78cdc566d6a
raon98 <cheol1998@kakao.com>
2025-05-14 16:50:02 +0900

    임시 로그인페이지로 강제이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
 import { redirect } from "next/navigation";
-import { getServerSession } from "next-auth";
 
 export default async function RootPage() {
-  const session = await getServerSession();
-
-  console.log(session);
-  // if (session) {
-  //   redirect("/chat");
-  // } else {
-  //   redirect("/login");
-  // }
+  redirect("/login");
 }
